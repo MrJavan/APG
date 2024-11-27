61e279bb2574661ff2b35874c5a9069db05e36b8
MrJavan <am1njavan@proton.me>
2024-11-27 14:34:12 +0330

    fix some bugs

--- main.py
+++ main.py
@@ -9,48 +9,34 @@ clear()
 print("- Specify the level of password security.")
 print("")
 print("[1] Secure Password - (Includes uppercase and lowercase letters)")
-print("[2] Secure Plus Password - (Includes uppercase and lowercase letters)")
-print("[3] Ultra Secure Password - (Includes uppercase and lowercase letters)")
+print("[2] Secure Plus Password - (Includes letters and numbers)")
+print("[3] Ultra Secure Password - (Includes letters, numbers and characters)")
 print("")
 
 
 
 def generate(first_user_input , second_user_input):
-    # if not first_user_input.isdigit() or 1 <= int(first_user_input) <= 3:
-    #     clear()
-    #     print("- Your input not valid.")
-    #     print("")
-    #     first_user_input = input("[?] Try Again :")
-    #     generate(first_user_input , second_user_input)
-
     if first_user_input == "1":
         clear()
         list = cwords + swords
         result = random.choices(list, k=int(second_user_input))
         print(''.join(result))
+        print("")
     elif first_user_input == "2":
         clear()
         list = cwords + swords + numbers
         result = random.choices(list, k=int(second_user_input))
         print(''.join(result))
+        print("")
     elif first_user_input == "3":
         clear()
         list = cwords + swords + numbers +chars
         result = random.choices(list, k=int(second_user_input))
         print('Generated Password: ',''.join(result))
-    # elif first_user_input not first_user_input.isdigit():
-
-    # else:
-    #     clear()
-    #     print("- Your input not valid.")
-    #     print("")
-    #     first_user_input = input("[?] Try Again :")
-    #     generate(first_user_input , second_user_input)
-
-
+        print("")
 
 def pwdlen (second_user_input):
-    if not second_user_input.isdigit() or int(second_user_input) < 8:
+    if not second_user_input.isdigit() or int(second_user_input) < 10:
         clear()
         print("The inserted password length is not secure.")
         print("")
@@ -64,15 +50,16 @@ def check(first_user_input):
         clear()
         print("- Your input not valid.")
         print("")
-        first_user_input = input("[?] Try Again :")
-        check(first_user_input)
-    # else:
-        
-    #     pwdlen(second_user_input)
+        first_user_input1 = input("[?] Try Again :")
+        first_user_input1 = check(first_user_input1)
+        return first_user_input1
+    else:
+        return first_user_input
 
+    
 
 first_user_input = input("[?] Select Number :")
-check(first_user_input)
+first_user_input = check(first_user_input)
 
 clear()
 
